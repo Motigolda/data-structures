14bcb9ea4c467d241718cc95229bf05f5259fc02
Motigolda <motigoldstin@gmail.com>
2021-08-24 21:05:47 +0300

    Added basic non-implemented methods to HashTable

--- HashTable.java
+++ HashTable.java
@@ -1,4 +1,31 @@
 
 public class HashTable {
-
+	private int[] data;
+	private boolean[] slotReserved; 
+	private final int LENGTH = 1000;
+	public HashTable() {
+		this.data = new int[LENGTH];
+		this.slotReserved = new boolean[LENGTH];
+		for (int i = 0; i < LENGTH; i++)
+			this.slotReserved[i] = false;
+	}
+	public void insert(int value) {
+		int key = this.hash(value);
+		
+	}
+	// returns the key of the value
+	// returns -1 if not found
+	
+	public int find(int value) {
+		return -1;
+	}
+	public void delete() {
+		
+	}
+	// returns the key of given value 
+	// Using linear probing
+	// Read about it here: https://en.wikipedia.org/wiki/Linear_probing
+	private int hash(int value) {
+		return -1;
+	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Welcome
+
 This repo contains implementations of data structures.
 In the current version, all the structures are get only integers as value.
